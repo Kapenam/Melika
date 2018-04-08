abc925fd0172a6588bfdead60240be2ee7e195b8
bethstevic <bethstevic89@gmail.com>
2018-04-08 15:38:18 -0400

    Set up Router, tests, and initial pages

---
+++ src/screens/App/screens/Leads/screens/Leads.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Leads = () => (
+  <div>
+    <h1>Melika</h1>
+
+    <p>Welcome to Melika. Just a lead page demo. Good bye!</p>
+  </div>
+)
+
+export default Leads
---
+++ src/screens/App/screens/Quotes/screens/Quotes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Quotes = () => (
+  <div>
+    <h1>Melika</h1>
+
+    <p>Welcome to Melika. Just a quote page demo. Good bye!</p>
+  </div>
+)
+
+export default Quotes
